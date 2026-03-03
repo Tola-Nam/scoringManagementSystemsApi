fb2add7a6164220bc7504dcd5bbace5b79e95e49
Tola Nam <namtola4444@gmail.com>
2026-03-03 16:33:12 +0700

    update pom

--- src/test/java/com/rupp/tola/dev/scoring_management_system/ScoringManagementSystemApplicationTests.java
+++ src/test/java/com/rupp/tola/dev/scoring_management_system/ScoringManagementSystemApplicationTests.java
@@ -2,31 +2,31 @@ package com.rupp.tola.dev.scoring_management_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.rupp.tola.dev.scoring_management_system.mapper.UserMapper;
-import com.rupp.tola.dev.scoring_management_system.repository.UsersRepository;
-import com.rupp.tola.dev.scoring_management_system.service.EmailService;
-import com.rupp.tola.dev.scoring_management_system.backup.Backup_JwtTokenUtil;
+// import com.rupp.tola.dev.scoring_management_system.mapper.UserMapper;
+// import com.rupp.tola.dev.scoring_management_system.repository.UsersRepository;
+// import com.rupp.tola.dev.scoring_management_system.service.EmailService;
+// import com.rupp.tola.dev.scoring_management_system.backup.Backup_JwtTokenUtil;
 
 @SpringBootTest(properties = "app.cors.allowed-origins=http://localhost:4200")
 class ScoringManagementSystemApplicationTests {
 
-	@MockBean
-	private UsersRepository userRepository;
+	// @MockBean
+	// private UsersRepository userRepository;
 
-	@MockBean
-	private PasswordEncoder passwordEncoder;
+	// @MockBean
+	// private PasswordEncoder passwordEncoder;
 
-	@MockBean
-	private EmailService emailService;
+	// @MockBean
+	// private EmailService emailService;
 
-	@MockBean
-	private UserMapper userMapper;
+	// @MockBean
+	// private UserMapper userMapper;
 
-	@MockBean
-	private Backup_JwtTokenUtil jwtTokenUtil;
+	// @MockBean
+	// private Backup_JwtTokenUtil jwtTokenUtil;
 
 //	@MockBean
 //	private PasswordService passwordService; // <<< Add this
